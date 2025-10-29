0f892ff518a24b62a7968c632fc5f22606489149
Joshua Sulouff <joshua.sulouff@techconsulting.tech>
2025-10-29 15:07:18 -0400

    Initial Commit

---
+++ CoreMLAssn/CoreMLAssnApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreMLAssnApp.swift
+//  CoreMLAssn
+//
+//  Created by Joshua Sulouff on 10/29/25.
+//
+
+import SwiftUI
+
+@main
+struct CoreMLAssnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoreMLAssnTests/CoreMLAssnTests.swift
@@ -0,0 +1,17 @@
+//
+//  CoreMLAssnTests.swift
+//  CoreMLAssnTests
+//
+//  Created by Joshua Sulouff on 10/29/25.
+//
+
+import Testing
+@testable import CoreMLAssn
+
+struct CoreMLAssnTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
